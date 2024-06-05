821bdc150617397ef25d7beb00f0e14d62b734c1
Gabriel P <124844227+GabrielP17@users.noreply.github.com>
2024-06-05 17:29:18 -0300

    Update projeto_hardware.py [NO LED]
    (Versão sem LED)

--- projeto_hardware.py
+++ projeto_hardware.py
@@ -4,21 +4,21 @@ from timeit import default_timer as timer
 rodadas = int(input("quantas rodadas você quer jogar? "))
 
 for i in range(rodadas):
-    print("quando o led acender aperte o botão o mais rapido possivel")
+    print("quando o aviso aparecer aperte ENTER o mais rapido possivel")
     time.sleep(0.5)
     print("espere .......")
     time.sleep(random.randint(2, 6))
     
     timer_start = timer()
     
-    input("aperte o botão")
+    input("******* AGORA ********")
     
     timer_end = timer()
     
     tempo_final = (timer_start - timer_end)
     
-    print(f"tempo = {'{:.3f}'.format(abs(tempo_final))}s")
+    print(f"seu tempo foi de = {'{:.3f}'.format(abs(tempo_final))}s")
     
     if i < rodadas - 1:
         print("---proxima rodada---")
-        time.sleep(1)
+        time.sleep(1.5)
